1070a7aacc77edfa0ea6b87a5048c4c113c2fe12
Aastha <aastha11110@gmail.com>
2020-06-05 06:07:29 +0000

    made about page

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ router.get("/",function(req,res){
 	res.render("landing");
 	// res.send("This will be the landing page soon");
 });
+router.get("/about",function(req,res){
+	res.render("about");
+});
 
 module.exports = router;
 	  
